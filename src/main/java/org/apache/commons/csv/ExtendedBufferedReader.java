b0016b212ace0aa641af8aec1b71afddea595e3c
Nokhaiz Ali <nokhaiz.ali@RT-LT-nokhaiz.rolustech.net>
2025-05-05 19:57:38 +0500

    Resolve reliability issues 3rd iteration

--- src/main/java/org/apache/commons/csv/ExtendedBufferedReader.java
+++ src/main/java/org/apache/commons/csv/ExtendedBufferedReader.java
@@ -253,7 +253,7 @@ final class ExtendedBufferedReader extends UnsynchronizedBufferedReader {
             if (current == CR) {
                 final int next = peek();
                 if (next == LF) {
-                    read(); // intentionally discard LF after CR
+                    read(); // Intentionally ignoring the value as it's not needed
                 }
             }
             if (current == EOF || current == LF || current == CR) {
